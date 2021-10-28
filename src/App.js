154529af83c96e241f4b8758bf88f8ef0e45e9a8
madhumita-chatne <madhumitachatne@gmail.com>
2021-10-28 17:51:46 +0530

    removed disconnect bug

--- src/App.js
+++ src/App.js
@@ -3,28 +3,19 @@ import io from 'socket.io-client';
 
 const App = () => {
     const [response, setResponse] = useState([]);
-    const [welcome, setWelcome] = useState("")
     let socket;
     useEffect(() => {
-         socket = io.connect("https://socket-backend-3towjekr2q-ue.a.run.app");
-        // socket = io.connect("http://localhost:4000")
+        //  socket = io.connect("http://localhost:4000");
+        socket = io.connect("https://socket-backend-3towjekr2q-ue.a.run.app");
 
-        socket.on("connect", data=>{
-         setWelcome("Welcome!")
-
-         setInterval(()=> setWelcome(""), 7000)
-        })
         socket.on("status", data => {
           setResponse(data);
           console.log("data", data)
         });
-
-        
       }, []);
 
   return(
       <div className = "App">
-        {welcome !== "" && <h2>{welcome}</h2>}
           Logs
           <ul>
          {response.map(resp =>{
